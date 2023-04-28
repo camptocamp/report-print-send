fd879ea02c63f880181bead1dae9699839b962da
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2023-04-28 14:46:12 +0200

    [ADD] pingen_env

---
+++ pingen_env/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "pingen.com server environment",
+    "version": "16.0.1.0.0",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Reporting",
+    "complexity": "easy",
+    "depends": ["pingen", "server_environment", "base_setup"],
+    "website": "https://github.com/OCA/report-print-send",
+    "data": [
+        "views/base_config_views.xml",
+    ],
+    "post_init_hook": "post_init_hook",
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
