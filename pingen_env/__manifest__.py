37706a18418209fa838e5238478850915546ac8d
Akim Juillerat <akim.juillerat@camptocamp.com>
2025-03-19 16:30:06 +0100

    Add myself to module maintainers

--- pingen_env/__manifest__.py
+++ pingen_env/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "pingen.com server environment",
     "version": "16.0.1.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
-    "maintainers": ["ajaniszewska-dev"],
+    "maintainers": ["ajaniszewska-dev", "grindtildeath"],
     "license": "AGPL-3",
     "category": "Reporting",
     "complexity": "easy",
